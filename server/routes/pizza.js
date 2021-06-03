818ae03a4f30d6e0e11bf7d5f27b4bfa212f36ad
Ezequiel Gresia <gresia.ezequiel@gmail.com>
2021-06-03 09:41:14 -0300

    revert

--- server/routes/pizza.js
+++ server/routes/pizza.js
@@ -1,7 +1,7 @@
 const router = require('express').Router();
 
 router.get('/', async (req, res) => {
-    res.json("all the pizzas displayed");
+    res.json("get pizzas");
 });
 
 router.post('/', async (req, res) => {

54b509d8048aac3b432b68a23f61cda16c2a1710
stefaniaalb <stefania.alb95@gmail.com>
2021-06-06 20:50:14 -0300

    changes in realizar pedido

--- src/components/RealizarPedido.vue
+++ src/components/RealizarPedido.vue
@@ -1,50 +1,87 @@
 <template>
-
-  <section class="src-components-realizar-pedido">
+  <section class="src-components-realizar-pedido justify-context-center">
     <div class="jumbotron">
       <div class="container">
-        <div class="row ">
+        <div class="row">
           <div class="col-10">
-            <h2 class="title1 mt-5">Detalles del pedido</h2> 
+            <h2 class="title1 mt-5">Detalles del pedido</h2>
           </div>
           <div class="col-2 col-offset-2">
-            <img src="../assets/logo tutti pizza.png" class="img-fluid img-size2 mt-2" alt="TuttiPizza logo">
+            <img
+              src="../assets/logo tutti pizza.png"
+              class="img-fluid img-size2 mt-2"
+              alt="TuttiPizza logo"
+            />
           </div>
         </div>
-      </div> 
-      <br>
-      <br>
-      
-      <table v-if="pizzas.length" class="table table-stripped">
+      </div>
+      <br />
+      <br />
+
+      <table class="table table-stripped" v-if="pizzas.length">
         <thead>
           <tr>
-          <th v-for="(col,index) in getCols" :key="index">{{col}}</th>
-          <th> cantidad </th>
+            <th
+              v-for="(col, index) in getCols"
+              :key="index"
+              style="background-color: #c0182f; color: white"
+            >
+              {{ col }}
+            </th>
           </tr>
         </thead>
         <tbody>
-        <tr v-for="(pizza, index) in pizzas" :key="index">
-          <th v-for="(col, index) in getCols" :key="index">{{pizza[col]}}</th>
-          <th>1</th>
-        </tr>
-        
-        
+          <tr v-for="pizza in pizzas" :key="pizza.id">
+            <td
+              v-for="(col, index) in getCols"
+              :key="col"
+              style="color: #424242; vertical-align: middle"
+            >
+              <span v-if="index < getCols.length - 1">
+                {{ pizza[col] }}
+              </span>
+              <span v-if="index === getCols.length - 1">
+                <button
+                  :disabled="pizza.cantidad === 0"
+                  class="btn btn-red"
+                  @click="restar(pizza)"
+                >
+                  -
+                </button>
+                {{ pizza.cantidad }}
+                <button class="btn btn-red" @click="sumar(pizza)">+</button>
+              </span>
+            </td>
+          </tr>
         </tbody>
       </table>
+
+      <div class="container">
+        <div class="row">
+          <div class="col-8"></div>
+          <div class="col-4">
+            <span style="font-weight: 600">Total: $ {{ getTotalPedido }}</span>
+          </div>
+        </div>
+      </div>
+
       <div class="form-group">
         <router-link to="/detallePedido">
-          <a type="button" class="btn btn-red btn-block">REALIZAR PEDIDO</a>
-          </router-link>
+          <a type="button" class="btn btn-red btn-block">
+            CONTINUAR
+          </a>
+        </router-link>
       </div>
+
       <div class="form-group">
         <router-link to="/home">
-          <a type="button" class="btn btn-red btn-block">VOLVER AL MENÚ PRINCIPAL</a>
+          <a type="button" class="btn btn-outline-secondary btn-block mb-5">
+            VOLVER AL MENÚ PRINCIPAL
+          </a>
         </router-link>
-          
       </div>
     </div>
   </section>
-
 </template>
 
 <script lang="js">
@@ -59,24 +96,45 @@
     data () {
       return {
         url: 'https://60aa8aef66f1d00017772f51.mockapi.io/pizzas/',
-        pizzas: []
+        pizzas: [],
       }
     },
 
     methods: {
       getPizzasAxios() {
         this.axios(this.url)
-        .then(respuesta => {
-          console.log(respuesta.data)
-          this.pizzas = respuesta.data
+        .then(({ data }) => {
+          console.log('data', data)
+          this.pizzas = data.map((pizza) => ({
+            ...pizza,
+            cantidad: 0,
+          }))
         })
         .catch(error => console.error(error))
+      },
+
+      sumar(pizza) {
+        pizza.cantidad++
+      },
+
+      restar(pizza) {
+        if(pizza.cantidad > 0){
+          pizza.cantidad--
+        } 
       }
     },
 
     computed: {
       getCols() {
         return Object.keys(this.pizzas[0])
+      },
+
+      getTotalPedido() {
+        let total = 0;
+        this.pizzas.forEach(({ cantidad, Precio }) => {
+          total += (parseInt(Precio) * cantidad)
+        });
+        return total
       }
     },
 
@@ -89,11 +147,6 @@
 </script>
 
 <style scoped lang="css">
-  .src-components-realizar-pedido {
-
-  }
-
-  
-
-  
+.src-components-realizar-pedido {
+}
 </style>

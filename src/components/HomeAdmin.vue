644df72a70e76f3327b8b05288df89b639371d8e
stefaniaalb <stefania.alb95@gmail.com>
2021-06-24 09:23:52 -0300

    greyscale to homeAdmin

--- src/components/HomeAdmin.vue
+++ src/components/HomeAdmin.vue
@@ -3,8 +3,8 @@
   <section class="src-components-home-admin">
     <div class="jumbotron">
       <div class="container d-flex flex-column justify-content-center align-items-center" style="height: 100vh">
-        <img src="../assets/logo tutti pizza.png" class="img-size mb-5" alt="TuttiPizza logo" />
-        <h4>ADMINISTRADOR</h4>
+        <img src="../assets/logo tutti pizza.png" class="img-size mb-5 blackandwhite" alt="TuttiPizza logo" />
+        <h4 style="color: #707070;">ADMINISTRADOR</h4>
         <div class="d-flex flex-column">
           <router-link to="/estadoPedidos">
             <a type="button" class="btn btn-secondary btn-block m-3">VER PEDIDOS</a>
@@ -46,4 +46,9 @@
   .src-components-home-admin {
 
   }
+
+  .blackandwhite{
+    -webkit-filter: grayscale(100%); /* Safari/Chrome */
+    filter: grayscale(100%);
+}
 </style>

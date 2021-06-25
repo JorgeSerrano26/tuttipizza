c04bce6f8c47389a1388bcd857e66bb07a613a16
Jorge Serrano <jorge.serrano@mercadolibre.com>
2021-06-25 09:17:11 -0300

    config

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,19 @@
 module.exports = {
-    publicPath: process.env.NODE_ENV === 'production'
-    ? '/tuttipizza'
-    : '/'
-}
+  publicPath: process.env.NODE_ENV === "production" ? "/tuttipizza" : "/",
+  devServer: {
+    compress: true,
+    disableHostCheck: true,
+  },
+  publicPath: "/tuttipizza/",
+  pages: {
+    index: {
+      // entry for the page
+      entry: "src/main.js",
+      // the source template
+      template: "public/index.html",
+      // output as dist/index.html
+      filename: "index.html",
+      title: "Home",
+    },
+  },
+};

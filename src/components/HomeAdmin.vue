55d9ad9e65847b39d256658af0320f0d4faf23ff
Ezequiel Gresia <gresia.ezequiel@gmail.com>
2021-06-30 19:47:10 -0300

    new margin in home-admin

--- src/components/HomeAdmin.vue
+++ src/components/HomeAdmin.vue
@@ -7,13 +7,13 @@
         <h4 style="color: #707070;">ADMINISTRADOR</h4>
         <div class="d-flex flex-column">
           <router-link to="/estadoPedidos">
-            <a type="button" class="btn btn-secondary btn-block m-3">VER PEDIDOS</a>
+            <a type="button" class="btn btn-secondary btn-block m-2">VER PEDIDOS</a>
           </router-link>
           <router-link to="/editarPizzas">
-            <a type="button" class="btn btn-secondary btn-block">VER PIZZAS</a>
+            <a type="button" class="btn btn-secondary btn-block m-2">VER PIZZAS</a>
           </router-link>
           <router-link to="/editarIngredientes">
-            <a type="button" class="btn btn-secondary btn-block">VER INGREDIENTS</a>
+            <a type="button" class="btn btn-secondary btn-block m-2">VER INGREDIENTES</a>
           </router-link>
         </div>
       </div>

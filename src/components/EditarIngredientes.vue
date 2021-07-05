9834ba82ed4e4bde58a56ec07b3f190b684ebd52
Ezequiel Gresia <gresia.ezequiel@gmail.com>
2021-07-05 13:06:33 -0300

    new display

--- src/components/EditarIngredientes.vue
+++ src/components/EditarIngredientes.vue
@@ -9,20 +9,15 @@
         </div>
       </header>
 
-
-    <table v-if="ingredients.length" class="table table-stripped">
+    <table v-if="ingredients.length" class="col-sm-6" style="float: left">
       <thead>
         <tr style="background-color: #c0182f; color: white">
-          <th>Tipo</th>
-          <th>Nombre</th>
+          <th>Masas</th>
           <th style="background-color: #c0182f; color: white" class="sticky-col first-col"></th>
         </tr>
       </thead>
       <tbody>
-        <tr v-for="ingredient in ingredients" :key="ingredient.id" style="vertical-align: middle">
-            <td>
-                {{ ingredient.typeIngredient }}
-            </td>
+        <tr v-for="ingredient in filterIngredient('dough')" :key="ingredient.id" style="vertical-align: middle">
             <td>
               {{ ingredient.name }}
             </td>
@@ -34,18 +29,79 @@
       </tbody>
     </table>
 
+    <table v-if="ingredients.length" class="col-sm-6" style="float: right">
+      <thead>
+        <tr style="background-color: #c0182f; color: white">
+          <th>Quesos</th>
+          <th style="background-color: #c0182f; color: white" class="sticky-col first-col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="ingredient in filterIngredient('cheese')" :key="ingredient.id" style="vertical-align: middle">
+            <td>
+              {{ ingredient.name }}
+            </td>
+            <div class="d-flex flex-row">
+              <!-- DELETE  -->
+              <button class="btn btn-red p-2" @click="borrar(ingredient._id)">BORRAR</button>
+            </div>
+        </tr>
+      </tbody>
+    </table>
+
+    <table v-if="ingredients.length" class="col-sm-6" style="float: left">
+      <thead>
+        <tr style="background-color: #c0182f; color: white">
+          <th>Toppings</th>
+          <th style="background-color: #c0182f; color: white" class="sticky-col first-col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="ingredient in filterIngredient('toppings')" :key="ingredient.id" style="vertical-align: middle">
+            <td>
+              {{ ingredient.name }}
+            </td>
+            <div class="d-flex flex-row">
+              <!-- DELETE  -->
+              <button class="btn btn-red p-2" @click="borrar(ingredient._id)">BORRAR</button>
+            </div>
+        </tr>
+      </tbody>
+    </table>
+
+    <table v-if="ingredients.length" class="col-sm-6" style="float: right">
+      <thead>
+        <tr style="background-color: #c0182f; color: white">
+          <th>Aceitunas</th>
+          <th style="background-color: #c0182f; color: white" class="sticky-col first-col"></th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="ingredient in filterIngredient('olives')" :key="ingredient.id" style="vertical-align: middle">
+            <td>
+              {{ ingredient.name }}
+            </td>
+            <div class="d-flex flex-row">
+              <!-- DELETE  -->
+              <button class="btn btn-red p-2" @click="borrar(ingredient._id)">BORRAR</button>
+            </div>
+        </tr>
+      </tbody>
+    </table>
+    </div>
+
     <ModalAgregarIngrediente v-show="this.$store.state.isIngredientAddModalVisible" :ingredientList="ingredients" />
 
     <!-- ADD  -->
-    <button type="button" class="btn btn-red"  @click="showAgregarModal">AGREGAR</button>
-
-    <div class="form-group">
-      <router-link to="/homeAdmin">
-        <a type="button" class="btn btn-red btn-block">VOLVER AL MENÚ DE ADMINISTRADOR</a>
-      </router-link>
-    </div>
-
-   </div>
+    <div class="col-sm-12" style="display: inline-block;">
+      <hr style="width:100%;text-align:left;margin-left:0">
+      <button type="button" class="btn btn-red"  @click="showAgregarModal">AGREGAR</button>
+        <div class="form-group">
+          <router-link to="/homeAdmin">
+            <a type="button" class="btn btn-red btn-block">VOLVER AL MENÚ DE ADMINISTRADOR</a>
+          </router-link>
+        </div>
+      </div>
   </section>
 </template>
 
@@ -100,6 +156,10 @@
       showAgregarModal() {
         this.$store.state.isIngredientAddModalVisible = true;
       },
+
+      filterIngredient(typeOfIngredient) {
+        return this.ingredients.filter((ingredient) => ingredient.typeIngredient === typeOfIngredient)
+      } 
     },
     computed: {
     },
